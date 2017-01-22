9298beb1ca80eefe8f44fc2cfae17d6ba1054f53
Łukasz Kaczmarek <luka.kaczmarek@gmail.com>
2017-01-22 18:21:10 +0000

    ss
    ss

---
+++ app/js/sample-controller.js
@@ -0,0 +1,26 @@
+angular.module('app')
+.controller('SampleController', ['$scope', function($scope) {
+ 
+  $scope.names = ['Joe', 'Marta', 'Lukasz'];
+
+  
+
+}]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
